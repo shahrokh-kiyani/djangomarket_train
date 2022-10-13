5ed9399871f0f6a7da6d7717cdf52cc38a35093f
Shahrokh Kiyani <shahrokhkiyani@proton.me>
2022-10-13 21:37:26 +0330

    add crispy forms to project

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # local Apps
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
+    
+    # Third party
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
