1f52528e7ffd7bd8e0df0dc819d67c36b6f96d4f
Shahrokh Kiyani <shahrokhkiyani@proton.me>
2022-10-19 20:17:14 +0330

    add StackedInline

--- products/admin.py
+++ products/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 
 from .models import Product, Comment
 
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    fields = ['author', 'text', 'stars', 'active']
+    extra = 0
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'status', 'price']
-
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['product' ,'author', 'active']
+    inlines = [CommentInline, ]
