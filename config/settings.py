c6079174d3cd699ae3a0d5ce1ba16db19321823d
shahrokhkiani <shahrokhkiani@proton.me>
2023-01-10 09:30:10 +0330

    fixed media files

--- config/settings.py
+++ config/settings.py
@@ -158,8 +158,8 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
